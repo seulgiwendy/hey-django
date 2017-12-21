758cfbe57ce81e0f37e82788c5e8e3457e110309
seulgiwendy <wheejuni@gmail.com>
2017-12-21 23:32:47 +0900

    [view] added polls listing template, views.py modification

--- survey/polls/views.py
+++ survey/polls/views.py
@@ -2,5 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from polls.models import Question
+
+
 def index(request):
-    return HttpResponse("hello, Seulgi!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list' : latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the question %s" % question_id)
+
+def results(request, question_id):
+    response = "Question id : %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote Page : %s" % question_id)
+
