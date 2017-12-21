33e790846d7203b097350aedde28239c34e22122
seulgiwendy <wheejuni@gmail.com>
2017-12-21 23:45:28 +0900

    [view] changed exception handling to django.shortcut 404 handling

--- survey/polls/views.py
+++ survey/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from polls.models import Question
@@ -11,7 +11,7 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    q = Question.objects.get(pk = question_id)
+    q = get_object_or_404(Question, pk = question_id)
     return render(request, "polls/detail.html", {'question' : q})
 
 def results(request, question_id):
